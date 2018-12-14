82386dcbb73a0db11721cabb1fe72e57312d5f0f
zdzulak <43354865+zdzulak@users.noreply.github.com>
2018-12-14 01:10:37 -0500

    test upload

---
+++ includes/global-nav.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="/index.php" title="Go to the Home page">Home</a></li>
+    <li><a href="/about.php" title="Learn about the game">About</a></li>
+    <li><a href="/contact.php" title="Talk with us about the game">Contact</a></li>
+    <li><a href="/contact.php" title="Learn about the comany behind the game">Nintendo</a></li>
+  </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Home | Nintendo Presents: Donkey Kong</title>
+		<meta name="author" content="Zackary Zulak" />
+		<meta name="description" content="Learn about Nintendo's hit new arcade game, Donkey Kong!">
+		<link rel="shortcut icon" href="images/dkicon.jpg" type="image/x-icon" />
+		<!--<link rel="stylesheet" href="css/styles.css" />-->
+	</head>
+
+  <body>
+    <header>
+      <img src="images/dklogo.png" height="100" width="275" alt="Donkey Kong Logo" />
+			<?php
+			require $_SERVER['DOCUMENT_ROOT'].'\includes\global-nav.php';
+			?>
+    </header>
+  </body>
+
+</html>
