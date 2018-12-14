8ce2be5eb790f4c97fcbcadacab87ec2de984b05
zdzulak <43354865+zdzulak@users.noreply.github.com>
2018-12-14 03:17:46 -0500

    Update includes/global-nav.php

--- includes/global-nav.php
+++ includes/global-nav.php
@@ -3,6 +3,6 @@
     <li><a href="/index.php" title="Go to the Home page">Home</a></li>
     <li><a href="/about.php" title="Learn about the game">About</a></li>
     <li><a href="/contact.php" title="Talk with us about the game">Contact</a></li>
-    <li><a href="/contact.php" title="Learn about the comany behind the game">Nintendo</a></li>
+    <li><a href="/nintendo.php" title="Learn about the company behind the game">Nintendo</a></li>
   </ul>
 </nav>
