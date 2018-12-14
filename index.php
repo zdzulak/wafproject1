a2ff8848dd43e14181042cf44b571dea4544f48e
zdzulak <43354865+zdzulak@users.noreply.github.com>
2018-12-14 01:39:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,17 @@
 			require $_SERVER['DOCUMENT_ROOT'].'\includes\global-nav.php';
 			?>
     </header>
+
+		<main>
+			<h3>Everyone's Going Ape Over Donkey Kong!</h3>
+			<p>"HELP! HELP!" cries the beautiful maiden as she is dragged up a labyrinth of structural beams by the ominous Donkey Kong. "SNORT. SNORT." Forboding music warns of the eventual doom that awaits the poor girl, lest she somehow be miraculously rescud. "But, wait! Fear not, fair maiden. Little Mario, the carpenter, is in hot pursuit of you this very moment."</p>
+
+			<p>Throwing fate to the wind, risking life and limb, or worse, little Mario tries desperately to climb the mighty fortress of steel, to save the lovely lady from the evil Mr. Kong. Little Mario must dodge all manner of obstacles - fireballs, plummeting beams and a barrage of exploding barrels fired at him by Donkey Kong. Admist the beautiful girl's constant pleas for help, your challenge is to maneuver little Mario up the steel structure, while helping him to avoid the rapid-fire succession of hazards that come his way.</p>
+
+			<p>As little Mario gallantly battles his way up the barriers, he is taunted and teased by Donkey Kong, who brazenly struts back and forth, beating his chest in joyful exuberance at the prospect of having the beautiful girl all to himself. It is your job to get little Mario to the top. For it is there, and only there, that he can send the mighty Donkey Kong to his mortal doom. Leaving little Mario and the beautiful girl to live happily ever after. "SIGH. SIGH."</p>
+
+			<p>So, if you want the most exciting, most fun-filled, most talked about family video game on the market, don't monkey around with anything but the original Donkey Kong.</p>
+		</main>
   </body>
 
 </html>
