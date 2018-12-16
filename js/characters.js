e9ca3230b6ca9d9679187d9d3244ec0b02e3ce82
zdzulak <43354865+zdzulak@users.noreply.github.com>
2018-12-16 18:36:14 -0500

    Update characters.js

--- js/characters.js
+++ js/characters.js
@@ -6,18 +6,18 @@ document.addEventListener("DOMContentLoaded", function(){
   for (var i=0; i<links.length; i++) {
 
     links[i].onclick = function(){
-
-      let source = this.getAttribute("href");
-
-      let placeholder = document.getElementById("placeholder");
-
-      placeholder.setAttribute("src",source);
-
-      let text = this.getAttribute("title");
-
-      let description = document.getElementById("description")
-
-      description.firstChild.nodeValue = text;
+      //
+      // let source = this.getAttribute("href");
+      //
+      // let placeholder = document.getElementById("placeholder");
+      //
+      // placeholder.setAttribute("src",source);
+      //
+      // let text = this.getAttribute("title");
+      //
+      // let description = document.getElementById("description")
+      //
+      // description.firstChild.nodeValue = text;
 
       return false;
 
